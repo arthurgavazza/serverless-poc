369698a64d9b77db142bd0b943082e1cf965d64a
arthurgavazza <arthurcarvalho_99@hotmail.com>
2023-07-15 15:42:53 -0300

    feat: graphql working

--- src/functions/users/handler.test.ts
+++ src/functions/users/handler.test.ts
@@ -1,44 +1,64 @@
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { getUsers } from './handler';
+
+import { AppSyncResolverEvent } from 'aws-lambda';
+import { getUsers, main } from './handler';
 import * as AWSMock from 'aws-sdk-mock';
+
+
+const mockData = {
+  Items: [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' },
+  ],
+};
 
 describe('getUsers', () => {
   afterEach(() => {
     AWSMock.restore('DynamoDB.DocumentClient');
   });
 
-  it('should return a list of users', async () => {
-    const mockData = {
-      Items: [
-        { id: 1, name: 'John Doe' },
-        { id: 2, name: 'Jane Smith' },
-      ],
-    };
-
+  beforeEach(() => {
     AWSMock.mock('DynamoDB.DocumentClient', 'scan', (_params, callback) => {
       callback(null, mockData);
     });
+  })
 
-    const result = await getUsers({} as APIGatewayProxyEvent , {} as Context);
-   
-    expect(result.statusCode).toEqual(200);
-    const {body: bodyStr} = result
-    const body = JSON.parse(bodyStr)
-    expect(JSON.stringify(body.getUsers)).toEqual(JSON.stringify(mockData.Items));
+  it('should return a list of users', async () => {
+    const result = await getUsers();
+    expect(result).toEqual(mockData.Items);
     
   });
+});
 
-  it('should return an error message when an error occurs', async () => {
-    const errorMessage = 'Database error';
+describe('main', () => {
+  afterEach(() => {
+    AWSMock.restore('DynamoDB.DocumentClient');
+  });
 
+  beforeEach(() => {
     AWSMock.mock('DynamoDB.DocumentClient', 'scan', (_params, callback) => {
-      callback(new Error(errorMessage));
+      callback(null, mockData);
     });
+  })
+  
+  it('should call getUsers when operation is "Query" and fieldName is "getUsers"', async () => {
+    const event = {
+      arguments: {},
+      info: { parentTypeName: 'Query', fieldName: 'getUsers' },
+    } as AppSyncResolverEvent<{ [key: string]: string | number }> ;
+    const result = await main(event);
+    expect(result).toBeDefined();
+    expect(Array.isArray(result)).toBe(true);
+  });
 
-    const result = await getUsers({} as APIGatewayProxyEvent , {} as Context);
-
-    expect(result.statusCode).toEqual(500);
-    expect(result.body).toEqual(JSON.stringify({ message: 'Ocorreu um erro ao buscar os usuários.' }));
-    
+  it('should throw an error for unknown operation', async () => {
+    const event = {
+      arguments: {},
+      info: { parentTypeName: 'Unknown', fieldName: 'Unknown' },
+    } as AppSyncResolverEvent<{ [key: string]: string | number }>;
+    try {
+      await main(event);
+    } catch (error) {
+      expect(error.message).toBe('unknown operation');
+    }
   });
 });
--- src/functions/users/handler.ts
+++ src/functions/users/handler.ts
@@ -1,38 +1,39 @@
+import { AppSyncResolverEvent } from 'aws-lambda'
 
-// import { middyfy } from '@libs/lambda';
-
-// const getUsers: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-//   return formatJSONResponse({
-//     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
-//     event,
-//   });
-// };
-
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
 import * as AWS from 'aws-sdk';
 
+type User = {
+  id:string;
+  name:string
+}
 
-
-export async function getUsers(_event:APIGatewayProxyEvent, _context: Context): Promise<APIGatewayProxyResult> {
+export async function getUsers(): Promise<User[]> {
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
-  try {
     const params = {
       TableName: 'users',
     };
 
     const data = await dynamoDb.scan(params).promise();
-
-    return  {
-      statusCode: 200,
-      body: JSON.stringify({getUsers: data.Items}),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: 'Ocorreu um erro ao buscar os usuários.' }),
-    };
-  }
+    return data.Items as User[]
 };
 
+const operations: { [key: string]: { [key: string]: Function } } = {
+  Query: { getUsers },
 
-export const main = getUsers;
+}
+
+export async function main(event: AppSyncResolverEvent<{ [key: string]: string | number }>){
+  const {
+    arguments: args,
+    info: { parentTypeName: typeName, fieldName },
+  } = event
+  const type = operations[typeName]
+  if (type) {
+    const operation = type[fieldName]
+    if (operation) {
+      return operation(args)
+    }
+  }
+  throw new Error('unknown operation')
+}
+
